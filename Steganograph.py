9d2e14b4ddc13c018126429cea0aaf11752e7713
Muneer Alam <77039639+Muneer320@users.noreply.github.com>
2024-10-09 23:59:27 +0530

    Update Steganograph.py

--- Steganograph.py
+++ Steganograph.py
@@ -35,6 +35,27 @@ def hideVideo():
         f.write(e.read())
     print("Done!")
 
+def hideFolder():
+    import zipfile
+    import os
+
+    folder = input("Folder to hide >>> ")
+    d_loc = input('Location of picture to hide in >>> ')
+    n_zip = folder + ".zip"
+    
+    with zipfile.ZipFile(n_zip, 'w') as z:
+        for root, _, files in os.walk(folder):
+            for file in files:
+                z.write(os.path.join(root, file))
+
+    with open(d_loc, 'ab') as f:
+        with open(n_zip, 'rb') as z:
+            f.write(z.read())
+
+    os.remove(n_zip)
+
+    print("Done!")
+
 
 def getHiddenImage():
     import PIL.Image as I
@@ -94,6 +115,27 @@ def getHiddenVideo():
 
     print("Done!")
 
+def getHiddenFolder():
+    import zipfile
+    import os
+
+    file = input("File to get hidden folder from >>> ")
+    n_folder = input("Name of new folder >>> ")
+    with open(file, 'rb') as f:
+        content = f.read()
+        offset = content.index(bytes.fromhex('FFD9'))
+        f.seek(offset + 2)
+
+        with open("temp.zip", 'wb') as z:
+            z.write(f.read())
+
+    with zipfile.ZipFile("temp.zip", 'r') as z:
+        z.extractall(n_folder)
+
+    os.remove("temp.zip")
+    print(f"Folder extracted as {n_folder}")
+
+
 def removeHiddenData():
     file = input("File to remove hidden data from >>> ")
     with open(file, 'rb') as f:
@@ -105,19 +147,45 @@ def removeHiddenData():
     print(f"Clean file saved as clean_{file}")
 
 
+
 def main():
     print("Welcome to Steganography!")
     print("1. Hide image")
     print("2. Hide message")
     print("3. Hide executable")
     print("4. Hide video")
-    print("5. Get hidden image")
-    print("6. Get hidden message")
-    print("7. Get hidden executable")
-    print("8. Get hidden video")
-    print("9. Remove hidden data")
+    print("5. Hide Folder")
+    print("6. Get hidden image")
+    print("7. Get hidden message")
+    print("8. Get hidden executable")
+    print("9. Get hidden video")
+    print("10. Get hidden folder")
+    print("11. Remove hidden data")
 
-    
+    funcs = {
+        '1': hideImage,
+        '2': hideMessage,
+        '3': hideExecutable,
+        '4': hideVideo,
+        '5': hideFolder,
+        '6': getHiddenImage,
+        '7': getHiddenMessage,
+        '8': getHiddenExecutable,
+        '9': getHiddenVideo,
+        '10': getHiddenFolder,
+        '11': removeHiddenData
+    }
+
+    choice = input(">>> ")
+    if choice in funcs:
+        funcs[choice]()
+    else:
+        print("Invalid choice. Exiting...")
+        r = input("Press r to restart >>> ")
+        if r == 'r':
+            main()
+
+'''
     match input(">>> "):
         case '1':
             hideImage()
@@ -128,20 +196,25 @@ def main():
         case '4':
             hideVideo()
         case '5':
-            getHiddenImage()
+            hideFolder()
         case '6':
-            getHiddenMessage()
+            getHiddenImage()
         case '7':
-            getHiddenExecutable()
+            getHiddenMessage()
         case '8':
-            getHiddenVideo()
+            getHiddenExecutable()
         case '9':
+            getHiddenVideo()
+        case '10':
+            getHiddenFolder()
+        case '11':
             removeHiddenData()
         case _:
             print("Invalid choice. Exiting...")
             r = input("Press r to restart >>> ")
             if r == 'r':
                 main()
+'''
 
 
 if __name__ == '__main__':
